ae6b998748024d5d72a926df0dcfb761184972ae
Niskarsh Mani Pandey <niskarshpandey0@gmail.com>
2026-04-30 11:53:40 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144552
+let accountEmail = "niskarsh@gamil.com"
+var accoutPassword ="12443"
+accountCity = "Goa"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hchc.com"
+accountPassword = "3233"
+accountCity = "lko"
+let accountState;
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail, accoutPassword,accountCity,accountState])
